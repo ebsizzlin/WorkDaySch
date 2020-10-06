ad7befc94877fe7ec0f866e6cd19da7e3061b398
Ella Thompson <fang98t@gmail.com>
2020-10-06 11:18:52 -0400

    timeblocks added and pseudocoding

---
+++ script.js
@@ -0,0 +1,6 @@
+//display current date and time using moment.js
+//color-coding for past present future
+//onclick time block to enter it
+//onclick save button to save it
+//data saved in local storage
+//data isn't reset after refresh
