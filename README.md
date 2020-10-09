4d36160f974549e2e38341351a25bbaf8410d19c
Ella Thompson <fang98t@gmail.com>
2020-10-09 00:53:41 -0400

    updated local storage js functions and html classes

--- README.md
+++ README.md
@@ -1,22 +1,17 @@
 # WorkDaySch - 5
 
-## Your Task
+## Description
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+A simple calendar application that allows a user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.
 
 ## User Story
 
-```
 AS AN employee with a busy schedule
 I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
-```
 
 ## Acceptance Criteria
 
-```
 GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
 THEN the current day is displayed at the top of the calendar
@@ -30,64 +25,28 @@ WHEN I click the save button for that time block
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
 THEN the saved events persist
-```
 
-## Mock-Up
+## Grading
 
-The following animation demonstrates the application functionality:
-
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
-
-## Grading Requirements
-
-This homework is graded based on the following criteria:
-
-### Technical Acceptance Criteria: 40%
-
-- Satisfies all of the above acceptance criteria plus the following:
-
-  - Uses the Moment.js library to work with date and time
-
-### Deployment: 32%
-
-- Application deployed at live URL
-
-- Application loads with no errors
-
-- Application GitHub URL submitted
-
-- GitHub repo contains application code
-
-### Application Quality: 15%
-
-- Application user experience is intuitive and easy to navigate
-
-- Application user interface style is clean and polished
-
-- Application resembles the mock-up functionality provided in the homework instructions
-
-### Repository Quality: 13%
-
-- Repository has a unique name
-
-- Repository follows best practices for file structure and naming conventions
-
-- Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-- Repository contains multiple descriptive commit messages
-
-- Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Description
-
-A simple calendar application that allows a user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.
+- 40% technical
+  - moment.js date / time
+- 32% deployment
+  - live, no errors, github url, code
+- 15% app quality
+  - intuitive, clean, resembles mock-up
+- 13% repo quality
+  - unique name, file structure, semantics, commits, readme
 
 ## Completed
 
 - html / css design
-- moment.js
+- live url
+- local storage function
+- color change function past / present / future
 
 ## Unfinished
+
+- moment.js date / time (error for dateNow)
 
 ## Screenshots
 
--- script.js
+++ script.js
@@ -1,43 +1,84 @@
-var dateNow = moment().format("MMMM Do YYYY, h:mm:ss");
-var hourNow = moment().format("H");
-var saveBtn = $(".button");
-var nine = $("#nine").val(9);
-var ten = $("#ten").val(10);
-var eleven = $("#eleven").val(11);
-var twelve = $("#twelve").val(12);
-var thirteen = $("#thirteen").val(13);
-var fourteen = $("#fourteen").val(14);
-var fifteen = $("#fifteen").val(15);
-var sixteen = $("#sixteen").val(16);
-var seventeen = $("#seventeen").val(17);
+//moment.js
+var timeNow = moment().format("H"); //24h clock
+var dateNow = moment().format("MMMM Do YYYY, h:mm:ss"); //rn says 'moment' is undefined
+console.log("timeNow:", timeNow);
 
-function changeTime() {
-  for (i = 0; i < this.length; i++)
-    //past
-    //if hourNow > dateNow
-    this[i].add("past");
-  this[i].remove("present");
-  this[i].remove("future");
-  //present
-  //if hourNow === dateNow
-  this[i].add("present");
-  this[i].remove("past");
-  this[i].remove("future");
-  //future
-  //if hourNow < dateNow
-  this[i].add("future");
-  this[i].remove("past");
-  this[i].remove("present");
+//display dateNow
+$("#currentDay").html(dateNow);
+
+//calender time variables
+var allTimes = {
+  "9h00": "",
+  "10h00": "",
+  "11h00": "",
+  "12h00": "",
+  "13h00": "",
+  "14h00": "",
+  "15h00": "",
+  "16h00": "",
+  "17h00": "",
+};
+
+//onclick to local storage functions - will stay displayed ?
+//way to make less repetative ?
+$(".b9").on("click", function () {
+  var entertext = $(".e9").val();
+  localStorage.setItem("9h00", entertext);
+});
+$(".b10").on("click", function () {
+  var entertext = $(".e19").val();
+  localStorage.setItem("10h00", entertext);
+});
+$(".b11").on("click", function () {
+  var entertext = $(".e11").val();
+  localStorage.setItem("11h00", entertext);
+});
+$(".b12").on("click", function () {
+  var entertext = $(".e12").val();
+  localStorage.setItem("12h00", entertext);
+});
+$(".b13").on("click", function () {
+  var entertext = $(".e13").val();
+  localStorage.setItem("13h00", entertext);
+});
+$(".b14").on("click", function () {
+  var entertext = $(".e14").val();
+  localStorage.setItem("14h00", entertext);
+});
+$(".b15").on("click", function () {
+  var entertext = $(".e15").val();
+  localStorage.setItem("15h00", entertext);
+});
+$(".b16").on("click", function () {
+  var entertext = $(".e16").val();
+  localStorage.setItem("16h00", entertext);
+});
+$(".b17").on("click", function () {
+  var entertext = $(".e17").val();
+  localStorage.setItem("17h00", entertext);
+});
+
+//function to change color of section based on time
+function changeColor() {
+  for (i = 0; i < allTimes.length; i++)
+    if (timeNow > dateNow) {
+      //past
+      timeNow[i].add("past");
+      timeNow[i].remove("present");
+      timeNow[i].remove("future");
+    } else if (timeNow === dateNow) {
+      //present
+      timeNow[i].add("present");
+      timeNow[i].remove("past");
+      timeNow[i].remove("future");
+    } else if (timeNow < dateNow) {
+      //futur
+      timeNow[i].add("future");
+      timeNow[i].remove("past");
+      timeNow[i].remove("present");
+    }
 }
 
-function saveBtn() {
-  localStorage.setItem;
-  //use this ?
-}
-
-//display current date and time using moment.js
-//for loop color-coding for past present future
-//onclick time block to enter it
-//onclick save button to save it
-//data saved in local storage
-//data isn't reset after refresh
+//invoking functions (is this smth i need to do ? no clue)
+changeColor();
+saveBtn();
